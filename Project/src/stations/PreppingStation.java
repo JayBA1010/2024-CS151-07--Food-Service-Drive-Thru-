503dec6986efc14c707db453c6ee13383592be9a
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:07:11 -0700

    Update PreppingStation.java

--- Project/src/stations/PreppingStation.java
+++ Project/src/stations/PreppingStation.java
@@ -1,6 +1,15 @@
 package stations;
 
+/**
+ * Represents a prepping station in the kitchen.
+ * This station is responsible for prepping orders 
+ */
 public class PreppingStation extends KitchenStation {
+    /**
+     * Constructs a new PreppingStation with predefined attributes.
+     * The station is named "Prepping Station" with 5 precedence and
+     * useDuration of 3 (ticks)
+     */
     public PreppingStation() {
         super("Prepping Station", 5, 3);
     }
