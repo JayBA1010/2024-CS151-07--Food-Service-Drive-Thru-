fa99276a2008f4ea9abaf7823b1b2cca54c7aefe
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 18:58:51 -0700

    Update GrilledChicken.java

--- Project/src/menuItems/GrilledChicken.java
+++ Project/src/menuItems/GrilledChicken.java
@@ -1,12 +1,24 @@
 package menuItems;
 
+/**
+ * The GrilledChicken class represents an order for grilled chicken in a drive-through simulation.
+ * It extends the Order class and initializes the order with a default price or a specified price.
+ */
 public class GrilledChicken extends Order {
+    /**
+     * Constructs a GrilledChicken order with a default price of $14.99.
+     */
     public GrilledChicken() {
         super("Grilled Chicken", 14.99);
 
         getTasks().add("Grilling Station");
     }
 
+    /**
+     * Constructs a GrilledChicken order with a specified price.
+     *
+     * @param price the price of the grilled chicken
+     */
     public GrilledChicken(double price) {
         super("Grilled Chicken", price);
 
