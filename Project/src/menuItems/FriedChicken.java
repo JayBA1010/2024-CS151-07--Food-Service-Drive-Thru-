1045c65e682ccc3ad0ba95510e0baee13a12a814
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 18:58:22 -0700

    Update FriedChicken.java

--- Project/src/menuItems/FriedChicken.java
+++ Project/src/menuItems/FriedChicken.java
@@ -1,12 +1,24 @@
 package menuItems;
 
+/**
+ * The FriedChicken class represents an order for fried chicken in a drive-through simulation.
+ * It extends the Order class and initializes the order with a default price or a specified price.
+ */
 public class FriedChicken extends Order {
+    /**
+     * Constructs a FriedChicken order with a default price of $9.99.
+     */
     public FriedChicken() {
         super("Fried Chicken", 9.99);
 
         getTasks().add("Frying Station");
     }
 
+    /**
+     * Constructs a FriedChicken order with a specified price.
+     *
+     * @param price the price of the fried chicken
+     */
     public FriedChicken(double price) {
         super("Fried Chicken", price);
 
