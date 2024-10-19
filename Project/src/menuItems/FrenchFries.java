e36ef4ee7d28669e8b759b2a536d5413f166eab5
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 18:57:38 -0700

    Update FrenchFries.java

--- Project/src/menuItems/FrenchFries.java
+++ Project/src/menuItems/FrenchFries.java
@@ -1,13 +1,24 @@
 package menuItems;
 
-
+/**
+ * The FrenchFries class represents an order for French fries in a drive-through simulation.
+ * It extends the Order class and initializes the order with a default price or a specified price.
+ */
 public class FrenchFries extends Order {
+    /**
+     * Constructs a FrenchFries order with a default price of $4.99.
+     */
     public FrenchFries() {
         super("French Fries", 4.99);
 
         getTasks().add("Frying Station");
     }
 
+    /**
+     * Constructs a FrenchFries order with a specified price.
+     *
+     * @param price the price of the French fries
+     */
     public FrenchFries(double price) {
         super("French Fries", price);
 
