b1d29a331046afe00b2579e798fff8efaf5e9f04
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:05:06 -0700

    Update ServeCustomer.java

--- Project/src/people/ServeCustomer.java
+++ Project/src/people/ServeCustomer.java
@@ -1,5 +1,9 @@
 package people;
 
+/**
+ * Represents the operations for serving customers in a drive-through system.
+ * This interface defines methods for managing customers, employees, and earnings.
+ */
 public interface ServeCustomer {
     void addCustomer(Customer customer);
 
