e308b0ddd14ee2bd33fecaa5b99f28e506c2a0bc
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:06:53 -0700

    Update PickupStation.java

--- Project/src/stations/PickupStation.java
+++ Project/src/stations/PickupStation.java
@@ -2,11 +2,27 @@ package stations;
 
 import people.Employee;
 
+/**
+ * Represents a pick up station in the kitchen.
+ * This station is responsible for handling picking up orders 
+ */
 public class PickupStation extends KitchenStation {
+    /**
+     * Constructs a new PickupStation with predefined attributes.
+     * The station is named "Pickup Station" with 4 precedence and
+     * useDuration of 1 (ticks)
+     */
     public PickupStation() {
         super("Pickup Station", 4, 1);
     }
 
+    /**
+     * Simulates an employee using the pickup station at a specific time.
+     * If the employee's time at the station is zero, the employee fulfills the order
+     * for the customer.
+     *
+     * @param time the current time tick
+     */
     @Override
     public void useStation(int time) {
         Employee employee = getEmployeeQueue().getFirst();
