9f312dba56afd2bb839f88ab67ecb02b2c498c92
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:00:10 -0700

    Update IceCream.java

--- Project/src/menuItems/IceCream.java
+++ Project/src/menuItems/IceCream.java
@@ -1,12 +1,24 @@
 package menuItems;
 
+/**
+ * The iceCream class represents an order for ice cream in a drive-through simulation.
+ * It extends the Order class and initializes the order with a default price or a specified price.
+ */
 public class IceCream extends Order {
+    /**
+     * Constructs an IceCream order with a default price of $7.99.
+     */
     public IceCream() {
         super("Ice Cream", 3.99);
 
         getTasks().add("Dessert Station");
     }
 
+    /**
+     * Constructs an IceCream order with a specified price.
+     *
+     * @param price the price of the ice cream
+     */
     public IceCream(double price) {
         super("Ice Cream", price);
 
