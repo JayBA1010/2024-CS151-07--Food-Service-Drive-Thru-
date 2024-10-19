e1fb36f2479b6b2f8f73eb80a0d237c45fabdff5
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:05:24 -0700

    Update DessertStation.java

--- Project/src/stations/DessertStation.java
+++ Project/src/stations/DessertStation.java
@@ -1,6 +1,15 @@
 package stations;
 
+/**
+ * Represents a dessert preparation station in the kitchen.
+ * This station is responsible for handling dessert orders 
+ */
 public class DessertStation extends KitchenStation {
+    /**
+     * Constructs a new DessertStation with predefined attributes.
+     * The station is named "Dessert Station" with 0 precedence and
+     * useDuration of 5 (ticks)
+     */
     public DessertStation() {
         super("Dessert Station", 0, 5);
     }
