fe14fbd161e122f64a71920c683165d5b2cb8192
JayBA1010 <jay.barriosabarquez@sjsu.edu>
2024-10-18 19:06:35 -0700

    Update OrderingStation.java

--- Project/src/stations/OrderingStation.java
+++ Project/src/stations/OrderingStation.java
@@ -5,18 +5,30 @@ import people.Employee;
 
 import java.util.LinkedList;
 
+/**
+ * Represents the ordering station in the kitchen where customers place their orders.
+ * This station handles customer interactions and manages the queue of customers
+ * waiting to be served
+ */
 public class OrderingStation extends KitchenStation {
     private final LinkedList<Customer> customerQueue;
 
+    /**
+     * Constructs an OrderingStation with a predefined name, precedence, and use duration.
+     * Initializes the customer queue for handling incoming orders.
+     */
     public OrderingStation() {
         super("Ordering Station", 3, 1);
         customerQueue = new LinkedList<>();
     }
 
-    public LinkedList<Customer> getCustomerQueue() {
-        return customerQueue;
-    }
-
+    /**
+     * Simulates an employee using the ordering station at a specific time.
+     * If there are customers in the queue and the employee's time at the station is zero,
+     * the employee processes the next customer's order.
+     *
+     * @param time the current time tick
+     */
     @Override
     public void useStation(int time) {
         Employee employee = getEmployeeQueue().getFirst();
@@ -26,5 +38,15 @@ public class OrderingStation extends KitchenStation {
         }
 
         super.useStation(time);
+    }
+
+    //Getters
+    /**
+     * Gets the queue of customers waiting to place their orders.
+     *
+     * @return the queue of customers
+     */
+    public LinkedList<Customer> getCustomerQueue() {
+        return customerQueue;
     }
 }
